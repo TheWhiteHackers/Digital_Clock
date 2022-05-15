66ed868cb9c0866ab4d0924c61c68e7a3d2908bf
TheWhiteHackers <nurefsanbera@gmail.com>
2022-05-15 10:56:39 -0500

    finished clock

--- script.js
+++ script.js
@@ -15,10 +15,15 @@ function showtime(){
 
     if (hour>12){hour=hour-12}
     
-
     console.log(hour, minute, timepass);
 
 
+    const ctime= hour + ":" + minute + " " + timepass;
+    console.log(ctime);
+
+    document.getElementById("myclock").innerText=ctime;
+
+    setTimeout(showtime, 1000)
 }
 
 showtime()
